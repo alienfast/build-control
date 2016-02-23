c8700a7c2eaf2ca3c708c5a30b404e9c73d39817
Kevin Ross <kevin.ross@alienfast.com>
2016-02-23 13:55:57 -0600

    working on build, test still comingling the cwd, need to remove it

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,16 +4,21 @@ import {Preset, Clean, Mocha, RollupEs, RollupCjs, EsLint, TaskSeries} from 'gul
 let preset = Preset.nodeSrc()
 
 // instantiate ordered array of recipes (for each instantiation the tasks will be created e.g. rollup:es and rollup:es:watch)
-let recipes = [
+let rollup = [
   new Clean(gulp, preset),
-  new EsLint(gulp, preset),
-  new Mocha(gulp, preset, {debug: false}),
-  [
-    new RollupEs(gulp, preset, {options: {dest: 'build-control.es.js'}}),
-    new RollupCjs(gulp, preset, {options: {dest: 'build-control.cjs.js'}})
-  ]
+  new RollupEs(gulp, preset, {options: {dest: 'build-control.es.js'}}),
+  new RollupCjs(gulp, preset, {options: {dest: 'build-control.cjs.js'}})
 ]
 
+let recipes = [
+  new EsLint(gulp, preset),
+  new Mocha(gulp, preset, {debug: false}),
+  rollup
+]
+
+// FIXME eat our own dogfood and use BuildControl to produce our own builds!
 
 // Simple helper to create the `default` and `default:watch` tasks as a sequence of the recipes already defined
-new TaskSeries(gulp, 'default', recipes)
+new TaskSeries(gulp, 'default', recipes, {debug: true})
+new TaskSeries(gulp, 'rollup', rollup)
+
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { default as BuildControl } from './build_control'
+export { default as Git } from './git'
