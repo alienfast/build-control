46d28c47311d631e8160aa61750e9c6cc4d554e0
Kevin Ross <kevin.ross@alienfast.com>
2016-03-17 20:11:17 -0500

    update to gulp-pipeline 4

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,22 @@
 import gulp from 'gulp'
-import {Prepublish, PublishBuild, Clean, EsLint, Mocha, Preset, RollupEs, RollupCjs, TaskSeries} from 'gulp-pipeline/src/index'
+import {Prepublish, PublishBuild, Clean, EsLint, Mocha, Preset, RollupEs, RollupCjs, Aggregate, series, parallel} from 'gulp-pipeline/src/index'
 
 let preset = Preset.nodeSrc()
 
 // instantiate ordered array of recipes (for each instantiation the tasks will be created e.g. rollup:es and rollup:es:watch)
-let rollup = [
+let rollup = series(gulp,
   new Clean(gulp, preset),
-  new RollupEs(gulp, preset, {options: {dest: 'build-control.es.js'}}),
-  new RollupCjs(gulp, preset, {options: {dest: 'build-control.cjs.js'}})
-]
+  parallel(gulp,
+    new RollupEs(gulp, preset, {options: {dest: 'build-control.es.js'}}),
+    new RollupCjs(gulp, preset, {options: {dest: 'build-control.cjs.js'}})
+  )
+)
 
-let recipes = [
+let recipes = series(gulp,
   new EsLint(gulp, preset),
   new Mocha(gulp, preset, {debug: false}),
   rollup
-]
+)
 
 let buildControlConfig = {
   debug: false,
@@ -24,9 +26,9 @@ let prepublish = new Prepublish(gulp, preset, buildControlConfig)
 let publishBuild = new PublishBuild(gulp, preset, buildControlConfig)
 
 // Simple helper to create the `default` and `default:watch` tasks as a sequence of the recipes already defined
-new TaskSeries(gulp, 'default', recipes, {debug: false})
-new TaskSeries(gulp, 'rollup', rollup)
-new TaskSeries(gulp, 'publish', [prepublish].concat(recipes).concat(publishBuild))
+new Aggregate(gulp, 'default', recipes, {debug: false})
+new Aggregate(gulp, 'rollup', rollup)
+new Aggregate(gulp, 'publish', series(gulp, prepublish, recipes,publishBuild))
 
 
 
